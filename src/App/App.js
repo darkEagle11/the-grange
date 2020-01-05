4047bdc7c0d3bd279cf6fc0cf4438d79ce93285e
Bema Konate <professorkonate@gmail.com>
2020-01-04 20:46:12 -0500

    Add async components

--- src/App/App.js
+++ src/App/App.js
@@ -5,14 +5,18 @@ import classes from './styles/App.css';
 import Layout from '../layout/layout';
 
 import HomeBuilder from '../home/pages/home/home';
-import BreakfastBuilder from '../menu/pages/breakfast/breakfast';
-import LunchBuilder from '../menu/pages/lunch/lunch';
-import DinnerBuilder from '../menu/pages/dinner/dinner';
-import DrinksBuilder from '../menu/pages/drinks/drinks';
-import ReserveBuilder from '../reserve/pages/reserve/reserve';
-import CartBuilder from '../order/pages/cart/cart';
-import FoodModal from '../order/pages/foodModal/foodModal';
-import PaymentBuilder from '../order/pages/payment/payment';
+import asyncComponent from '../shared/components/asyncComponent/asyncComponent';
+
+const BreakfastBuilder = asyncComponent(() => import('../menu/pages/breakfast/breakfast'));
+const LunchBuilder = asyncComponent(() => import('../menu/pages/lunch/lunch'));
+const DinnerBuilder = asyncComponent(() => import('../menu/pages/dinner/dinner'))
+const DrinksBuilder = asyncComponent(() => import('../menu/pages/drinks/drinks'))
+const ReserveBuilder = asyncComponent(() => import('../reserve/pages/reserve/reserve'));
+const CartBuilder = asyncComponent(() => import('../order/pages/cart/cart'));
+const FoodModal = asyncComponent(() => import('../order/pages/foodModal/foodModal'));
+const PaymentBuilder = asyncComponent(() => import('../order/pages/payment/payment'));
+
+
 
 class App extends Component {
   render() {
